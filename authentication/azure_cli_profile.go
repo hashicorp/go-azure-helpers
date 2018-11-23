b4b1010eec1df863fcd394f8c91101789fc24f0d
tombuildsstuff <git@ibuildstuff.co.uk>
2018-11-23 11:19:38 +0100

    refactor: removing a duplicate call

--- authentication/azure_cli_profile.go
+++ authentication/azure_cli_profile.go
@@ -32,12 +32,6 @@ func (a *azureCLIProfile) populateFields() error {
 		}
 	}
 
-	// now we know the Subscription ID & Tenant ID we can find the associated Client ID/Access Token
-	err := a.populateClientIdAndAccessToken()
-	if err != nil {
-		return err
-	}
-
 	// always pull the environment from the Azure CLI, since the Access Token's associated with it
 	return a.populateEnvironment()
 }
