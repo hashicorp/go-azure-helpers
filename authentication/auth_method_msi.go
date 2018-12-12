be8ba0cd7018ff27ee086aabb3f715155c70cc61
Laurent Lesle <laurentmac@Laurents-MBP.lan>
2018-12-12 14:11:10 +0000

    adding support for user MSI authentication

--- authentication/auth_method_msi.go
+++ authentication/auth_method_msi.go
@@ -11,6 +11,7 @@ import (
 
 type managedServiceIdentityAuth struct {
 	endpoint string
+	clientID string
 }
 
 func (a managedServiceIdentityAuth) build(b Builder) (authMethod, error) {
@@ -27,6 +28,7 @@ func (a managedServiceIdentityAuth) build(b Builder) (authMethod, error) {
 
 	auth := managedServiceIdentityAuth{
 		endpoint: endpoint,
+		clientID: b.ClientID,
 	}
 	return auth, nil
 }
@@ -39,13 +41,27 @@ func (a managedServiceIdentityAuth) name() string {
 	return "Managed Service Identity"
 }
 
-func (a managedServiceIdentityAuth) getAuthorizationToken(oauthConfig *adal.OAuthConfig, endpoint string) (*autorest.BearerAuthorizer, error) {
-	spt, err := adal.NewServicePrincipalTokenFromMSI(a.endpoint, endpoint)
-	if err != nil {
-		return nil, err
+func (a managedServiceIdentityAuth) getAuthorizationToken(oauthConfig *adal.OAuthConfig, resource string) (*autorest.BearerAuthorizer, error) {
+	log.Printf("[DEBUG] getAuthorizationToken using MSI endpoint %q", a.endpoint)
+	log.Printf("[DEBUG] getAuthorizationToken using client_id %q", a.clientID)
+	log.Printf("[DEBUG] Calling getAuthorizationToken for resource: %q", resource)
+
+	if a.clientID == "" {
+		spt, err := adal.NewServicePrincipalTokenFromMSI(a.endpoint, resource)
+		if err != nil {
+			return nil, err
+		}
+		auth := autorest.NewBearerAuthorizer(spt)
+		return auth, nil
+	} else {
+		spt, err := adal.NewServicePrincipalTokenFromMSIWithUserAssignedID(a.endpoint, resource, a.clientID)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get oauth token from MSI for user assigned identity: %v", err)
+		}
+		
+		auth := autorest.NewBearerAuthorizer(spt)
+		return auth, nil
 	}
-	auth := autorest.NewBearerAuthorizer(spt)
-	return auth, nil
 }
 
 func (a managedServiceIdentityAuth) populateConfig(c *Config) error {
