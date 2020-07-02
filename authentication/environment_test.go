8ab793e48cda1b7caac0c9864f960182dc4eba6c
Matthew Frahry <mbfrahry@gmail.com>
2020-07-02 05:53:58 -0700

    More tests

--- authentication/environment_test.go
+++ authentication/environment_test.go
@@ -24,25 +24,30 @@ func TestAzureEnvironmentNames(t *testing.T) {
 }
 
 func TestAccAzureEnvironmentByNameFromEndpoint(t *testing.T) {
-	c, err := BuildAuthClient()
-	if err != nil {
-		t.Fatalf("Error building client: %s", err)
-	}
-
-	_, err = AzureEnvironmentByNameFromEndpoint(context.TODO(), c.MetadataURL, c.Environment)
+	_, err := AzureEnvironmentByNameFromEndpoint(context.TODO(), "management.azure.com", "public")
 	if err != nil {
 		t.Fatalf("Error getting Endpoint: %s", err)
+	}
+	_, err = AzureEnvironmentByNameFromEndpoint(context.TODO(), "management.azure.com", "AzureCloud")
+	if err != nil {
+		t.Fatalf("Error getting Endpoint: %s", err)
+	}
+	_, err = AzureEnvironmentByNameFromEndpoint(context.TODO(), "badurl", "AzureCloud")
+	if err == nil {
+		t.Fatal("Expected error from bad endpoint")
+	}
+	_, err = AzureEnvironmentByNameFromEndpoint(context.TODO(), "management.azure.com", "badEnvironment")
+	if err == nil {
+		t.Fatal("Expected error from bad environment")
 	}
 }
 
 func TestAccIsEnvironmentAzureStack(t *testing.T) {
-	c, err := BuildAuthClient()
-	if err != nil {
-		t.Fatalf("Error building client: %s", err)
-	}
-
-	_, err = IsEnvironmentAzureStack(context.TODO(), c.MetadataURL, c.Environment)
+	ok, err := IsEnvironmentAzureStack(context.TODO(),"management.azure.com", "public")
 	if err != nil {
 		t.Fatalf("Error getting Endpoint: %s", err)
+	}
+	if ok {
+		t.Fatal("Expected `public` environment to not be Azure Stack")
 	}
 }
