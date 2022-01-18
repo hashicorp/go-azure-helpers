1655a588630ea09d0e002a4e3ce5540d9a94e8d5
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-01-18 12:47:47 +0100

    resourcemanager/commonids: regenerating the Resource Group ID

--- resourcemanager/commonids/resource_group.go
+++ resourcemanager/commonids/resource_group.go
@@ -9,11 +9,13 @@ import (
 
 var _ resourceids.ResourceId = ResourceGroupId{}
 
+// ResourceGroupId is a struct representing the Resource ID for a Resource Group
 type ResourceGroupId struct {
 	SubscriptionId    string
 	ResourceGroupName string
 }
 
+// NewResourceGroupID returns a new ResourceGroupId struct
 func NewResourceGroupID(subscriptionId string, resourceGroupName string) ResourceGroupId {
 	return ResourceGroupId{
 		SubscriptionId:    subscriptionId,
@@ -21,6 +23,7 @@ func NewResourceGroupID(subscriptionId string, resourceGroupName string) Resourc
 	}
 }
 
+// ParseResourceGroupID parses 'input' into a ResourceGroupId
 func ParseResourceGroupID(input string) (*ResourceGroupId, error) {
 	parser := resourceids.NewParserFromResourceIdType(ResourceGroupId{})
 	parsed, err := parser.Parse(input, false)
@@ -42,6 +45,8 @@ func ParseResourceGroupID(input string) (*ResourceGroupId, error) {
 	return &id, nil
 }
 
+// ParseResourceGroupIDInsensitively parses 'input' case-insensitively into a ResourceGroupId
+// note: this method should only be used for API response data and not user input
 func ParseResourceGroupIDInsensitively(input string) (*ResourceGroupId, error) {
 	parser := resourceids.NewParserFromResourceIdType(ResourceGroupId{})
 	parsed, err := parser.Parse(input, true)
@@ -63,20 +68,38 @@ func ParseResourceGroupIDInsensitively(input string) (*ResourceGroupId, error) {
 	return &id, nil
 }
 
+// ValidateResourceGroupID checks that 'input' can be parsed as a Resource Group ID
+func ValidateResourceGroupID(input interface{}, key string) (warnings []string, errors []error) {
+	v, ok := input.(string)
+	if !ok {
+		errors = append(errors, fmt.Errorf("expected %q to be a string", key))
+		return
+	}
+
+	if _, err := ParseResourceGroupID(v); err != nil {
+		errors = append(errors, err)
+	}
+
+	return
+}
+
+// ID returns the formatted Resource Group ID
 func (id ResourceGroupId) ID() string {
 	fmtString := "/subscriptions/%s/resourceGroups/%s"
 	return fmt.Sprintf(fmtString, id.SubscriptionId, id.ResourceGroupName)
 }
 
+// Segments returns a slice of Resource ID Segments which comprise this Resource Group ID
 func (id ResourceGroupId) Segments() []resourceids.Segment {
 	return []resourceids.Segment{
-		resourceids.StaticSegment("subscriptions", "subscriptions", "subscriptions"),
+		resourceids.StaticSegment("staticSubscriptions", "subscriptions", "subscriptions"),
 		resourceids.SubscriptionIdSegment("subscriptionId", "12345678-1234-9876-4563-123456789012"),
-		resourceids.StaticSegment("resourceGroups", "resourceGroups", "resourceGroups"),
+		resourceids.StaticSegment("staticResourceGroups", "resourceGroups", "resourceGroups"),
 		resourceids.ResourceGroupSegment("resourceGroupName", "example-resource-group"),
 	}
 }
 
+// String returns a human-readable description of this Resource Group ID
 func (id ResourceGroupId) String() string {
 	components := []string{
 		fmt.Sprintf("Subscription: %q", id.SubscriptionId),
--- resourcemanager/commonids/validation.go
+++ resourcemanager/commonids/validation.go
@@ -16,20 +16,6 @@ func ValidateManagementGroupID(input interface{}, key string) (warnings []string
 	return
 }
 
-func ValidateResourceGroupID(input interface{}, key string) (warnings []string, errors []error) {
-	v, ok := input.(string)
-	if !ok {
-		errors = append(errors, fmt.Errorf("expected %q to be a string", key))
-		return
-	}
-
-	if _, err := ParseResourceGroupID(v); err != nil {
-		errors = append(errors, err)
-	}
-
-	return
-}
-
 func ValidateUserAssignedIdentityID(input interface{}, key string) (warnings []string, errors []error) {
 	v, ok := input.(string)
 	if !ok {
