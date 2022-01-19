80921482bc309bcea0f41ef4398c4fe518e54d22
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-01-19 18:53:11 +0100

    commonschema: adding support for Extended Location
    This is a kind of location which has more dynamic names than for the location
    field as such we'll have to leave this with lax validation for now.

--- resourcemanager/commonschema/location.go
+++ resourcemanager/commonschema/location.go
@@ -3,7 +3,19 @@ package commonschema
 import (
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
+
+func ExtendedLocation() *schema.Schema {
+	return &schema.Schema{
+		Type:             schema.TypeString,
+		Required:         true,
+		ForceNew:         true,
+		StateFunc:        location.StateFunc,
+		DiffSuppressFunc: location.DiffSuppressFunc,
+		ValidateFunc:     validation.StringIsNotEmpty,
+	}
+}
 
 func Location() *schema.Schema {
 	return &schema.Schema{
