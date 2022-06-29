b9ccb0a0081ebffdf0c25adef9fabd8fbb880667
Steph <steph@hashicorp.com>
2022-06-29 09:04:09 +0200

    check for 404 in poller

--- polling/poller.go
+++ polling/poller.go
@@ -3,7 +3,9 @@ package polling
 import (
 	"context"
 	"fmt"
+	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"net/http"
+	"strings"
 
 	"github.com/Azure/go-autorest/autorest"
 	"github.com/Azure/go-autorest/autorest/azure"
@@ -16,13 +18,31 @@ type LongRunningPoller struct {
 	future *azure.Future
 	ctx    context.Context
 	client autorest.Client
+	method string
 }
 
 // NewLongRunningPollerFromResponse creates a new LongRunningPoller from the HTTP Response
+// this is deprecated and replaced by NewPollerFromResponse. Can be removed once all the
+// embedded SDKs have been removed.
 func NewLongRunningPollerFromResponse(ctx context.Context, resp *http.Response, client autorest.Client) (LongRunningPoller, error) {
 	poller := LongRunningPoller{
 		ctx:    ctx,
 		client: client,
+	}
+	future, err := azure.NewFutureFromResponse(resp)
+	if err != nil {
+		return poller, err
+	}
+	poller.future = &future
+	return poller, nil
+}
+
+// NewPollerFromResponse creates a new LongRunningPoller from the HTTP Response
+func NewPollerFromResponse(ctx context.Context, resp *http.Response, client autorest.Client, method string) (LongRunningPoller, error) {
+	poller := LongRunningPoller{
+		ctx:    ctx,
+		client: client,
+		method: method,
 	}
 	future, err := azure.NewFutureFromResponse(resp)
 	if err != nil {
@@ -39,6 +59,11 @@ func (fw *LongRunningPoller) PollUntilDone() error {
 	}
 
 	err := fw.future.WaitForCompletionRef(fw.ctx, fw.client)
+	if strings.EqualFold(fw.method, "DELETE") {
+		if !response.WasNotFound(fw.future.Response()) {
+			return err
+		}
+	}
 	fw.HttpResponse = fw.future.Response()
 	return err
 }
