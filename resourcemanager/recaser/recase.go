4565705712c32b6dffcf394f784fef002e662183
jackofallops <ste@hashicorp.com>
2024-06-25 10:14:44 +0100

    add support for error returns to recasing utility add support for retrieving registered ID map add support for prepending supplied ID with '/' - some Azure API's incorrectly return values without it, but that shouldn't be the user's problem

--- resourcemanager/recaser/recase.go
+++ resourcemanager/recaser/recase.go
@@ -19,18 +19,22 @@ func ReCase(input string) string {
 // reCaseWithIds tries to determine the type of Resource ID defined in `input` to be able to re-case it based on an input list of Resource IDs
 func reCaseWithIds(input string, ids map[string]resourceids.ResourceId) string {
 	output := input
-	recased := false
+	reCased := false
+	var parseError error
 
 	key, ok := buildInputKey(input)
 	if ok {
 		id := ids[*key]
 		if id != nil {
-			output, recased = parseId(id, input)
+			output, parseError = parseId(id, input)
+			if parseError == nil {
+				reCased = true
+			}
 		}
 	}
 
-	// if we can't find a matching id recase these known segments
-	if !recased {
+	// if we can't find a matching id re-case these known segments
+	if !reCased {
 
 		segmentsToFix := []string{
 			"/subscriptions/",
@@ -47,8 +51,32 @@ func reCaseWithIds(input string, ids map[string]resourceids.ResourceId) string {
 	return output
 }
 
+func RecaseKnownId(input string) (*string, error) {
+	return reCaseKnownId(input, knownResourceIds)
+}
+
+func reCaseKnownId(input string, ids map[string]resourceids.ResourceId) (*string, error) {
+	output := input
+
+	key, ok := buildInputKey(input)
+	if ok {
+		id := ids[*key]
+		if id != nil {
+			var parseError error
+			output, parseError = parseId(id, input)
+			if parseError != nil {
+				return &output, fmt.Errorf("fixing case for ID '%s': %+v", input, parseError)
+			}
+		}
+	} else {
+		return nil, fmt.Errorf("could not determine ID type for '%s', or ID type not supported", input)
+	}
+
+	return &output, nil
+}
+
 // parseId uses the specified ResourceId to parse the input and returns the id string with correct casing
-func parseId(id resourceids.ResourceId, input string) (string, bool) {
+func parseId(id resourceids.ResourceId, input string) (string, error) {
 
 	// we need to take a local copy of id to work against else we're mutating the original
 	localId := id
@@ -56,15 +84,15 @@ func parseId(id resourceids.ResourceId, input string) (string, bool) {
 	parser := resourceids.NewParserFromResourceIdType(localId)
 	parsed, err := parser.Parse(input, true)
 	if err != nil {
-		return input, false
+		return input, err
 	}
 
 	if err = id.FromParseResult(*parsed); err != nil {
-		return input, false
+		return input, err
 	}
 	input = id.ID()
 
-	return input, true
+	return input, err
 }
 
 // fixSegment searches the input id string for a specified segment case-insensitively
@@ -81,9 +109,13 @@ func fixSegment(input, segment string) string {
 // so it can be used as a key to extract the correct id from knownResourceIds
 func buildInputKey(input string) (*string, bool) {
 
-	// don't attempt to build a key if this isn't a standard resource id
+	// Attempt to determine if this is just missing a leading slash and prepend it if it seems to be
 	if !strings.HasPrefix(input, "/") {
-		return nil, false
+		if len(input) == 0 || !strings.Contains(input, "/") {
+			return nil, false
+		}
+
+		input = "/" + input
 	}
 
 	output := ""
